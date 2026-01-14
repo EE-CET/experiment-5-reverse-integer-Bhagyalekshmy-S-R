d79050bc1cba83c629e9e4ad1fed6b66c73f844a
Your Name <you@example.com>
2026-01-14 21:33:44 +0530

    first commit

--- src/ReverseInteger.java
+++ src/ReverseInteger.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class ReverseInteger { 
-        // TODO: Read an integer and print its reverse  
+     public static int reverse(int number){
+               int n=number;
+               int rev=0;
+               while(n!=0){
+                    int a=n%10;
+                    rev= rev*10 + a;
+                    n=n/10;
+                    }
+                 return rev;
+              }
+
+  public static void main(String args[]){
+         Scanner sc = new Scanner(System.in);
+         int num=sc.nextInt();
+         System.out.println(reverse(num));
+}       
 }
